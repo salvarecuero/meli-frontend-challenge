fdb8196cf6fe1e1210a26ea6d7d4632a0cf275eb
Salvador Recuero <salvarecuero@gmail.com>
2021-05-08 13:04:02 -0300

    Added BreadCrumb in DetailedItem

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./styles/BreadCrumb.scss";
 
-function BreadCrumb({ categories }) {
-  console.log(categories);
-  if (categories.length) {
+function BreadCrumb({ categories, category_id }) {
+  let [statedCategories, setStatedCategories] = useState(categories);
+
+  useEffect(() => {
+    if (!statedCategories?.length) {
+      getCategories();
+    }
+  });
+
+  async function getCategories() {
+    return await fetch(`
+    http://localhost:3500/api/categories/${category_id}
+    `)
+      .then((response) => response.json())
+      .then((data) => setStatedCategories(data));
+  }
+
+  if (statedCategories?.length) {
     return (
       <div className="breadcrumb-container">
-        {categories.map((category) => {
+        {statedCategories.map((category) => {
           return (
-            <>
+            <div key={category}>
               <Link
                 className="breadcrumb-category"
                 to="/"
                 onClick={(e) => e.preventDefault()}
-                key={category}
               >
                 {category}
               </Link>{" "}
               {" > "}
-            </>
+            </div>
           );
         })}
       </div>
--- src/components/DetailedItem.js
+++ src/components/DetailedItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles/DetailedItem.scss";
+import BreadCrumb from "./BreadCrumb";
 
 function DetailedItem({ item }) {
   const conditions = {
@@ -7,35 +8,39 @@ function DetailedItem({ item }) {
     used: "Usado",
     not_specified: "",
   };
+
   return (
-    <div className="detailed-item-container">
-      <div className="detailed-item-picture-desc-column">
-        <img
-          className="detailed-item-picture"
-          src={item.picture}
-          alt={item.title}
-        />
-        <span className="detailed-item-desc-title">
-          Descripción del producto
-        </span>
-        <span className="detailed-item-desc">{item.description}</span>
+    <>
+      <BreadCrumb category_id={item.category_id} />
+      <div className="detailed-item-container">
+        <div className="detailed-item-picture-desc-column">
+          <img
+            className="detailed-item-picture"
+            src={item.picture}
+            alt={item.title}
+          />
+          <span className="detailed-item-desc-title">
+            Descripción del producto
+          </span>
+          <span className="detailed-item-desc">{item.description}</span>
+        </div>
+        <div className="detailed-item-data-column">
+          <span className="detailed-item-condition-sold">
+            {item.condition ? conditions[item.condition] : ""} -{" "}
+            {item.sold_quantity}{" "}
+            {item.sold_quantity !== 1 ? "vendidos" : "vendido"}
+          </span>
+          <strong className="detailed-item-title">{item.title}</strong>
+          <span className="detailed-item-price">
+            $ {item.price.amount.toLocaleString()}
+            <sup className="detailed-item-price-decimals">
+              {item.price.decimals}
+            </sup>
+          </span>{" "}
+          <button className="detailed-item-buy-button">Comprar</button>
+        </div>
       </div>
-      <div className="detailed-item-data-column">
-        <span className="detailed-item-condition-sold">
-          {item.condition ? conditions[item.condition] : ""} -{" "}
-          {item.sold_quantity}{" "}
-          {item.sold_quantity !== 1 ? "vendidos" : "vendido"}
-        </span>
-        <strong className="detailed-item-title">{item.title}</strong>
-        <span className="detailed-item-price">
-          $ {item.price.amount.toLocaleString()}
-          <sup className="detailed-item-price-decimals">
-            {item.price.decimals}
-          </sup>
-        </span>{" "}
-        <button className="detailed-item-buy-button">Comprar</button>
-      </div>
-    </div>
+    </>
   );
 }
 
